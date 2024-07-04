519057f5d611a929318fd6495c3188a6fb822d39
Chuckshallcode <charleycooling@gmail.com>
2024-07-04 18:29:59 -0500

    commit

---
+++ projects.java/src/main/java/projects/service/ProjectService.java
@@ -0,0 +1,12 @@
+package projects.service;
+
+import projects.dao.ProjectsDao;
+import projects.entity.Project;
+
+public class ProjectService {
+	private ProjectsDao projectDao = new ProjectsDao();
+	
+	public Project addProject(Project project) {
+		return projectDao.insertProject(project);
+	}
+}
